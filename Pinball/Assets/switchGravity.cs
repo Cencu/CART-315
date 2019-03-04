3c4fa193858ec67e23a765e4d4f469f66683798a
Cencu <31831899+Cencu@users.noreply.github.com>
2019-03-04 17:19:34 -0500

    added variable "gravity" to ball
    the mass of the ball changes in an attempt to simulate variable gravity. the ball changes mass every 5 seconds, from being really light to heavy.

---
+++ Pinball/Assets/switchGravity.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class switchGravity : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startingTime = 5f;
+    public float mass;
+    public Rigidbody rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startingTime;
+        rb = GetComponent<Rigidbody>();
+        rb.mass = mass;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        if (currentTime <= 0 )
+        {
+            mass = Random.Range(0, 10);
+            currentTime = 5;
+
+        }
+        Debug.Log(mass);
+
+    }
+}

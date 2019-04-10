96a7aca1e50a627e951a2aa22120e48383f18c26
Cencu <31831899+Cencu@users.noreply.github.com>
2019-04-10 00:44:48 -0400

    added readme file as c# script

---
+++ Pinball/Assets/pinball/README.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class README : MonoBehaviour
+{
+    //DOCUMENTATION
+    /* There are scripts where I did use tutorials as part of my base code
+     * https://www.youtube.com/watch?v=1FhVMFMG97Q
+     * https://www.youtube.com/watch?v=gmH-t68jnKk&list=LLboXMYg5njLjM0INgllL4JQ&index=2&t=0s
+     * https://www.youtube.com/watch?v=P7Gaf85MXBY&list=LLboXMYg5njLjM0INgllL4JQ&index=3&t=0s
+     * https://www.youtube.com/watch?v=iCMhrKOuBZg&list=LLboXMYg5njLjM0INgllL4JQ&index=6&t=0s
+     * https://www.youtube.com/watch?v=Ouu3D_VHx9o&list=LLboXMYg5njLjM0INgllL4JQ&index=7&t=0s
+     * https://www.youtube.com/watch?v=tFRk6lgOZG4&list=LLboXMYg5njLjM0INgllL4JQ&index=8&t=0s
+     * https://www.youtube.com/watch?v=y7WgV8-yfcI&list=LLboXMYg5njLjM0INgllL4JQ&index=10&t=0s
+     * https://www.youtube.com/watch?v=BXTjsCERdsw&list=LLboXMYg5njLjM0INgllL4JQ&index=9&t=0s
+
+        ASSETS
+        https://www.turbosquid.com/FullPreview/Index.cfm/ID/1067758
+        https://www.turbosquid.com/FullPreview/Index.cfm/ID/1141022
+        https://www.turbosquid.com/FullPreview/Index.cfm/ID/1373706
+        https://www.turbosquid.com/FullPreview/Index.cfm/ID/616773
+        https://www.turbosquid.com/FullPreview/Index.cfm/ID/542531
+
+
+    */
+}
